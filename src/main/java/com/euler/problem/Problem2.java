d107fe4fde132136b28171da06f3929811f5dff8
Loic Beylot <loic.beylot@gmail.com>
2020-08-01 19:21:48 +0100

    Improved solution that only calculate even numbers.

--- src/main/java/com/euler/problem/Problem2.java
+++ src/main/java/com/euler/problem/Problem2.java
@@ -2,13 +2,13 @@ package com.euler.problem;
 
 public class Problem2 {
     public int fiboEvenSum(int max) {
-        int fib1 = 1;
+        int fib1 = 0;
         int fib2 = 2;
-        int newFibValue = fib1 + fib2;
+        int newFibValue = 4 * fib2 + fib1;
         int evenSum = 0;
         while (newFibValue < max) {
-            evenSum += fib2 * ((fib2 + 1) % 2);
-            newFibValue = fib1 + fib2;
+            evenSum += fib2;
+            newFibValue = 4 * fib2 + fib1;
             fib1 = fib2;
             fib2 = newFibValue;
         }
